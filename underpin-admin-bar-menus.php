3f40637656a9da52b98433f94f9e1764d5221cd5
Alex Standiford <a@alexstandiford.com>
2021-05-03 16:23:29 -0700

    Fixes issues where extended plugins did not get registered items.

--- underpin-admin-bar-menus.php
+++ underpin-admin-bar-menus.php
@@ -4,13 +4,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $class ) {
-	if ( 'Underpin\Underpin' === $class ) {
-		require_once( plugin_dir_path( __FILE__ ) . 'Admin_Bar_Menu.php' );
-		require_once( plugin_dir_path( __FILE__ ) . 'Admin_Bar_Menu_Instance.php' );
-		Underpin\underpin()->loaders()->add( 'admin_bar_menus', [
-			'instance' => 'Underpin_Admin_Bar_Menu\Abstracts\Admin_Bar_Menu',
-			'default'  => 'Underpin_Admin_Bar_Menu\Factories\Admin_Bar_Menu_Instance',
-		] );
-	}
+add_action( 'underpin/before_setup', function ( $instance ) {
+	require_once( plugin_dir_path( __FILE__ ) . 'Admin_Bar_Menu.php' );
+	require_once( plugin_dir_path( __FILE__ ) . 'Admin_Bar_Menu_Instance.php' );
+	$instance->loaders()->add( 'admin_bar_menus', [
+		'instance' => 'Underpin_Admin_Bar_Menu\Abstracts\Admin_Bar_Menu',
+		'default'  => 'Underpin_Admin_Bar_Menu\Factories\Admin_Bar_Menu_Instance',
+	] );
 } );
