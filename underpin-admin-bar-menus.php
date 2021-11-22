63f14bc2e5637a1527feaa87068f611c1921140b
Alex Standiford <a@alexstandiford.com>
2021-11-21 19:00:33 -0600

    Compat for underpin 2

--- underpin-admin-bar-menus.php
+++ underpin-admin-bar-menus.php
@@ -1,14 +1,19 @@
 <?php
+
+use Underpin\Abstracts\Underpin;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $file, $class ) {
-	require_once( plugin_dir_path( __FILE__ ) . 'Admin_Bar_Menu.php' );
-	require_once( plugin_dir_path( __FILE__ ) . 'Admin_Bar_Menu_Instance.php' );
-	Underpin\underpin()->get( $file, $class )->loaders()->add( 'admin_bar_menus', [
-		'instance' => 'Underpin_Admin_Bar_Menu\Abstracts\Admin_Bar_Menu',
-		'default'  => 'Underpin_Admin_Bar_Menu\Factories\Admin_Bar_Menu_Instance',
-	] );
-}, 10, 2 );
+Underpin::attach( 'setup', new \Underpin\Factories\Observer( 'admin_bar_menus', [
+	'update' => function ( Underpin $plugin ) {
+		require_once( plugin_dir_path( __FILE__ ) . 'Admin_Bar_Menu.php' );
+		require_once( plugin_dir_path( __FILE__ ) . 'Admin_Bar_Menu_Instance.php' );
+		$plugin->loaders()->add( 'admin_bar_menus', [
+			'instance' => 'Underpin_Admin_Bar_Menu\Abstracts\Admin_Bar_Menu',
+			'default'  => 'Underpin_Admin_Bar_Menu\Factories\Admin_Bar_Menu_Instance',
+		] );
+	},
+] ) );
