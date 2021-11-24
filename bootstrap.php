d0ec79dbbdab0cde2be590626889d210b9f0247c
Alex Standiford <a@alexstandiford.com>
2021-11-24 15:28:42 -0600

    Fixes loader call

--- bootstrap.php
+++ bootstrap.php
@@ -9,6 +9,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 // Add this loader.
 Underpin::attach( 'setup', new Loader('admin_bar_menus', [
-	'instance' => 'Underpin\Admin_Bar_Menu\Abstracts\Admin_Bar_Menu',
-	'default'  => 'Underpin\Admin_Bar_Menu\Factories\Admin_Bar_Menu_Instance',
+	'abstraction_class' => 'Underpin\Admin_Bar_Menu\Abstracts\Admin_Bar_Menu',
+	'default_factory'   => 'Underpin\Admin_Bar_Menu\Factories\Admin_Bar_Menu_Instance',
 ] ) );
